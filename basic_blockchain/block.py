78e4c8638400a08ecf31b567cf51324cd5b42971
Danhogg <51833556+Danhogg@users.noreply.github.com>
2020-11-07 17:24:24 +0000

    Add files via upload

--- basic_blockchain/block.py
+++ basic_blockchain/block.py
@@ -3,8 +3,8 @@ from  hashlib import sha256
 # creating a class of block for making new blocks in the blockchain
 class Block:
     #default constructor for block
-    def __init__(self, transactoins, previous_hash, nonce = 0):
-        self.transactions = transactoins
+    def __init__(self, transactions, previous_hash, nonce = 0):
+        self.transactions = transactions
         self.previous_hash = previous_hash
         self.nonce = nonce 
         self.timestamp = datetime.now()
